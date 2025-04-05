cc6552e34a03eb377cbbfcf2857b053b60437f69
Yegor Bugayenko <yegor256@gmail.com>
2025-04-05 09:20:36 +0800

    #93 cov fix

--- test/test__helper.rb
+++ test/test__helper.rb
@@ -6,6 +6,13 @@
 $stdout.sync = true
 
 require 'simplecov'
+SimpleCov.external_at_exit = true
 SimpleCov.start
 
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 require 'minitest/autorun'
+
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
